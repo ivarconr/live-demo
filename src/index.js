6dae84d8f9512f3630f60e6e600c85d7fe764d9a
Ivar Conradi Østhus <ivar@getunleash.ai>
2023-12-15 09:04:12 +0100

    use frontend api again

--- src/index.js
+++ src/index.js
@@ -14,13 +14,11 @@ if(!userId) {
 
 
 const config = {
-  // url: 'https://app.unleash-hosted.com/demo/api/frontend',
-  // clientKey: 'live-demo:default.59a3a6a3cd5eec326027cab68deea1e2c9a0230f0c2ada37cc9d046c',
-  url: 'https://app.unleash-hosted.com/demo/api/proxy',
-  clientKey: 'proxy-123',
-  refreshInterval: 2,
+  url: 'https://app.unleash-hosted.com/demo/api/frontend',
+  clientKey: 'live-demo:default.59a3a6a3cd5eec326027cab68deea1e2c9a0230f0c2ada37cc9d046c',
+  refreshInterval: 10,
   appName: 'react-app',
-  headerName: 'x-api-key',
+  // headerName: 'x-api-key',
   context: { userId },
 };
 
